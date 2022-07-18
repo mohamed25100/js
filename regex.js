faf556bc646b5c9ac04c7321473566e4eeda0401
Mohamed BOUCHERBA <107109761+mohamed25100@users.noreply.github.com>
2022-07-18 09:32:58 +0200

    Create regex.js form input a-z 0-9 only

---
+++ regex.js
@@ -0,0 +1,10 @@
+$('input').bind('input', function() {
+  var c = this.selectionStart,
+      r = /[^a-z0-9 .]/gi,
+      v = $(this).val();
+  if(r.test(v)) {
+    $(this).val(v.replace(r, ''));
+    c--;
+  }
+  this.setSelectionRange(c, c);
+});
